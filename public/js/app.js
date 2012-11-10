503af3b890858f0dc03c2bb0fe72c89b72f76f67
Thomas Bates <hola@ilovemonsters.co.uk>
2012-11-10 14:23:03 +0000

    Adding some js

---
+++ public/js/app.js
@@ -0,0 +1,23 @@
+$( document ).ready( function() {
+  
+  // Bind a click event to each link within the
+  // essay part of the page.
+  $(".essay a").click(function ( event ){ 
+    // Grab link that has been clicked.
+    // and find its href attribute
+    // save to two variables.
+    var el = $( event.target ),
+        link = el.attr("href");
+    
+    // Also create a shorthand for our iframe.
+    var iframe = $( ".iframe iframe");
+    
+    // Swap original src with link variable.
+    iframe.attr("src", link);
+    
+    // Prevent default link behaviour
+    event.preventDefault();
+    event.stopPropagation();
+  });
+  
+});
